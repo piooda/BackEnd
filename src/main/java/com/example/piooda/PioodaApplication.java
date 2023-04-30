b7e09f79e4bfd1688b3dbbf7d180d55737e55e40
seeunjang <wisjang1@naver.com>
2023-04-30 18:38:47 +0900

    [feat] 초기 세팅

---
+++ src/main/java/com/example/piooda/PioodaApplication.java
@@ -0,0 +1,13 @@
+package com.example.piooda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/piooda/PioodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piooda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PioodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
