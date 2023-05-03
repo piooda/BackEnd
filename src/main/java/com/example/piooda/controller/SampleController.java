4e69038d4e62142ff5431129fe0293e7cb6bf0ff
seeunjang <wisjang1@naver.com>
2023-05-04 03:54:56 +0900

    [feat] git ignoer + annotation 변경

--- src/main/java/com/example/piooda/controller/SampleController.java
+++ src/main/java/com/example/piooda/controller/SampleController.java
@@ -1,12 +1,13 @@
 package com.example.piooda.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
-    @RequestMapping("/sample")
+    @GetMapping("/sample")
     public String sample() {
         return "Sample!!!!!!!!";
     }
